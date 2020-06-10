3e83bbf53e78f705a2dc73e4e8e894ecc293d25c
michel van veldhoven <michel.vanveldhoven@planet.nl>
2020-06-10 23:07:56 +0200

    Add project files.

---
+++ ClientUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientUI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "NSeviceBus Client UI";
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+            Console.WriteLine("Press Enter to exit!");
+            Console.ReadLine();
+
+            await endpointInstance.Stop();
+        }
+    }
+}
