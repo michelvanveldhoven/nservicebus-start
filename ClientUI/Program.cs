6262a3a14a2cceaa1611f2534d6084511caf57ab
michel van veldhoven <michel.vanveldhoven@planet.nl>
2020-06-10 23:53:10 +0200

    added message library and message handler

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using NServiceBus;
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -6,6 +8,8 @@ namespace ClientUI
 {
     class Program
     {
+        static ILog log = LogManager.GetLogger<Program>();
+
         static async Task Main(string[] args)
         {
             Console.Title = "NSeviceBus Client UI";
@@ -17,7 +21,38 @@ namespace ClientUI
             Console.WriteLine("Press Enter to exit!");
             Console.ReadLine();
 
+            await RunLoop(endpointInstance);
+
             await endpointInstance.Stop();
+        }
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit the program");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+                        log.Info($"Sending PlaceOrder command, OrderId {command.OrderId}");
+                        await endpointInstance.SendLocal(command);
+                        break;
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        log.Info("Unknown command. Please try again");
+                        break;
+                }
+
+            }
         }
     }
 }
